4c6293547ebe1dd5b3435dfb96935949527ccb2c
Peter Magnusson <kmpm@birchroad.net>
2015-10-13 08:19:47 +0200

    some fixes

--- test/reader.test.js
+++ test/reader.test.js
@@ -1,4 +1,5 @@
 var Code = require('code');   // assertion library
+var expect = Code.expect;
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 
@@ -10,23 +11,27 @@ var through2 = require('through2');
 
 
 lab.experiment('reader', function () {
-	lab.test('jox', {timeout: 30000}, function (done) {
-		
-
+	lab.test('jox',  function (done) {
+		var count = 0;
 		function t (chunk, encoding, next) {
 			var s = JSON.stringify(chunk) + '\n';
 			this.push(s);
+			count++;
 			next();
 		}
-
+		var reader = new lib.Reader();
+		var ws = fs.createWriteStream(__dirname + '/out/small.json');
 		var rs = fs.createReadStream(__dirname + '/fixtures/small.sl2')
-		.pipe(new lib.Reader())
+		.pipe(reader)
 		.pipe(through2.obj(t))
 		//.pipe(new stream.Transform({transform: t}))
-		.pipe(fs.createWriteStream(__dirname + '/out/small.json'));
+		.pipe(ws);
 
-		rs.on('end', function () {
+		ws.on('finish', function () {
+			expect(count).to.equal(4017);
+			console.log('ws finish');
 			done();
 		});
+
 	});
 });
